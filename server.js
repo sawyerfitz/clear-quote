4b5b9f954d9c0cdd46ce6423f6d9f5dc82c26efa
Sawyer FitzRandolph <sawyerfitzrandolph@Sawyers-MacBook-Air-4.local>
2026-04-15 22:48:56 -0400

    remove duplicate createServer

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const http = require('http');
 const https = require('https');
 
 http.createServer((req, res) => {
-http.createServer((req, res) => {
   if (req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('ok');
